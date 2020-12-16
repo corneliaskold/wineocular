0bda2044e682ca034873c6a1ade53c9215732ffb
Cornelia Sköld <cornelia.skold@gmail.com>
2020-12-16 19:45:55 +0100

    testat göra java-objekt till json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,19 @@ import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) {
+        port(2020);
         Gson gson = new Gson();
 
-        get("/hello", (req, res) -> "Hello World");
+        get("/", (req, res) -> {
+
+            //jag bara testade att göra java-objekt till json
+            Recipe recipe = new Recipe();
+            //TODO: hämta recept
+            recipe.title = "Spaghetti";
+            recipe.description = "How to make spaghetti";
+
+            return gson.toJson(recipe, Recipe.class);
+        });
+
     }
 }
---
+++ src/main/java/Recipe.java
@@ -0,0 +1,9 @@
+public class Recipe {
+    public String title = null;
+    public String description = null;
+
+    public Recipe(){
+
+    }
+
+}
