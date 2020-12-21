dc23581d9c7f9322a9f5b9ae4c79d2509ed76ed6
th da <theresa_dannberg@hotmail.com>
2020-12-21 18:35:14 +0100

    FoodInSeason
    Class FoodInSeason

---
+++ src/main/java/CurrentMonth.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+
+import static java.time.Month.*;
+
+public class CurrentMonth {
+
+    HashMap<Month, String> englishToSwedish = new HashMap<>();
+
+    public CurrentMonth() {
+        addToHashMap();
+    }
+
+    // Stores english-swedish translations in the hashMap
+    public void addToHashMap() {
+        englishToSwedish.put(JANUARY, "januari");
+        englishToSwedish.put(FEBRUARY, "februari");
+        englishToSwedish.put(MARCH, "mars");
+        englishToSwedish.put(APRIL, "april");
+        englishToSwedish.put(MAY, "maj");
+        englishToSwedish.put(JUNE, "juni");
+        englishToSwedish.put(JULY, "juli");
+        englishToSwedish.put(AUGUST, "augusti");
+        englishToSwedish.put(SEPTEMBER, "september");
+        englishToSwedish.put(OCTOBER, "oktober");
+        englishToSwedish.put(NOVEMBER, "november");
+        englishToSwedish.put(DECEMBER, "december");
+    }
+
+    // Returns the current month as a string in swedish
+    public String getCurrentMonth() {
+        Month month = LocalDate.now().getMonth();
+        return englishToSwedish.get(month);
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println(new CurrentMonth().getMonth());
+//    }
+
+}
