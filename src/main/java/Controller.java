242c604914446c05163888818ab7de93e3de54e4
Cornelia Sköld <cornelia.skold@gmail.com>
2021-01-11 18:40:32 +0100

    Kommentarer i controller och Recipe

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,18 +1,36 @@
 import java.util.ArrayList;
 
+/**
+ * The type Controller.
+ */
 public class Controller {
     private RecipeGetter recipeGetter;
     private FoodInSeason foodInSeason;
 
+    /**
+     * Instantiates a new Controller.
+     */
     public Controller() {
         foodInSeason = new FoodInSeason();
         recipeGetter = new RecipeGetter();
     }
 
+    /**
+     * Gets recipe by id.
+     *
+     * @param id the id of the recipe
+     * @return the recipe by id as Recipe object
+     */
     public Recipe getRecipeById(int id) {
         return recipeGetter.getById(id);
     }
 
+    /**
+     * Gets recipe array.
+     *
+     * @param wine the wine
+     * @return an ArrayList with Recipe matching wine
+     */
     public ArrayList<Recipe> getRecipeArray(String wine) {
         System.out.println("Controller getRecipeArray(): " + wine);
         for (String s : getFoodInSeason()){
@@ -23,6 +41,11 @@ public class Controller {
         return recipeGetter.getByWineAndSeason(wine, getFoodInSeason());
     }
 
+    /**
+     * Gets food in season.
+     *
+     * @return ingredients as String object, in an ArrayList
+     */
     public ArrayList<String> getFoodInSeason() {
         return foodInSeason.getIngredientsInSeason();
     }
--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 
+/**
+ * Recipe is used to store data of a recipe, in a format that is able to convert into JSON using Gson.
+ */
 public class Recipe {
+
     public String title;
     public String[] ingredients;
     public String description;
@@ -8,12 +12,29 @@ public class Recipe {
     public int id;
     public ArrayList<String> instructions;
 
+    /**
+     * Instantiates a new Recipe.
+     *
+     * @param title    the title
+     * @param imageURL the image url
+     * @param id       the id
+     */
     public Recipe(String title, String imageURL, int id){
         this.title = title;
         this.imageURL = imageURL;
         this.id = id;
     }
 
+    /**
+     * Instantiates a new Recipe.
+     *
+     * @param title        the title
+     * @param imageURL     the image url
+     * @param id           the id
+     * @param description  the description
+     * @param ingredients  the ingredients
+     * @param instructions the instructions
+     */
     public Recipe(String title, String imageURL, int id, String description, String[] ingredients, ArrayList<String> instructions) {
         this.title = title;
         this.imageURL = imageURL;
@@ -23,18 +44,36 @@ public class Recipe {
         this.instructions = instructions;
     }
 
+    /**
+     * Instantiates a new Recipe.
+     *
+     * @param title the title
+     */
     public Recipe(String title) {
         this.title = title;
     }
 
+    /**
+     * Instantiates a new Recipe.
+     */
     public Recipe() {
 
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets title.
+     *
+     * @return the title
+     */
     public String getTitle() {
         return title;
     }
