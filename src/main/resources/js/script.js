0f4baca3516b70ac70802400e8df527dbb6d080b
jool <noobjool@gmail.com>
2021-01-11 13:18:12 +0100

    no message

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -9,15 +9,20 @@
       var output = "";
       for (var i=0; i<json.length; i++) {
         var recept = json[i];
-        output += '<div class="result-item">';
+        output += '<div class="result-item" onclick="load_recept(\'' + recept.details + '\')">';
         output += '<div class="polaroid">';
         output += '<div class="result-image"><img src="' + recept.imageURL + '"></div>';
-        output += '<div class="result-title" ><h3>' + recept.title + '</h3></div>'
-        output += '</div>'
-        output += '</div>'
+        output += '<div class="result-title"><h3>' + recept.title + '</h3></div>';
+        output += '</div>';
+        output += '</div>';
       }
       $('#results').html(output);
     });
+  }
+
+  function load_recept(url) {
+    console.log("searching url " + url);
+    
   }
 
 
